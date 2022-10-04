933424cf5a363d25033081d5c1f44184af4d3d9e
singh-navdeep1 <singhnavdeep350@gmail.com>
2022-10-04 15:32:42 +0530

    sss

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
- 
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+var delay = 1000; //in milleseconds
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
+jQuery(document).ready(function($){
+  setTimeout(function(){ showNewsletterPopup(); }, delay);
+  
+  $('.popup-close').click(function(){
+      $('.newsletter-overlay').hide();
+      
+      //when closed create a cookie to prevent popup to show again on refresh
+      setCookie('newsletter-popup', 'popped', 30);
   });
+});
+
+function showNewsletterPopup(){
+  if( getCookie('newsletter-popup') == ""){
+     $('.newsletter-overlay').show();
+     setCookie('newsletter-popup', 'popped', 30);
+  }
+  else{
+    console.log("Newsletter popup blocked.");
+  }
+}
+
+
+function setCookie(cname,cvalue,exdays)
+{
+    var d = new Date();
+    d.setTime(d.getTime()+(exdays*24*60*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = cname+"="+cvalue+"; "+expires+"; path=/";
+}
+
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) 
+    {
+        var c = jQuery.trim(ca[i]);
+        if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+
